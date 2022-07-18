7dadae5c7e2ead0e78cba66e621cf397f1ae9543
SwarnitSinha <swarnsinha786@gmail.com>
2022-07-18 19:54:36 +0530

    1st commit for frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState,useEffect} from "react";
+import io from "socket.io-client";
+import {nanoid} from "nanoid";
+
+
+
+const socket = io.connect("http://localhost:5000");
+const userName = nanoid(4);
+
 function App() {
+
+  const [message,setMessage] = useState("");
+  const [chat,setChat] = useState([]); //we will put value to the array
+
+  const sendChat = (e)=>{
+    e.preventDefault();
+    socket.emit("chat",{message,userName});
+    // console.log("here ff");f
+    setMessage("");
+  }
+
+  useEffect(()=>{
+    socket.on("chat",(payload)=>{
+      console.log("here ");
+      setChat([...chat,payload]);
+    })
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Let's talk</h1>
+        <div class="container">
+            
+            {chat.map( (payload,index) =>{
+              let varibaleClass = "send";
+              if(payload.userName==userName){
+                varibaleClass = "send";
+              }
+              else{
+                varibaleClass = "receive";
+              }
+              return (
+                
+                <p key={index} className={varibaleClass} msg>
+                  {payload.message} : 
+                  <span>id : {payload.userName}</span>
+                </p>
+              );
+            })}
+
+        </div>
+        {/* {chat.map((payload,index) =>{
+          return (
+            <p key={index}>
+              {payload.message} :
+              <span>id : {payload.userName}</span>
+            </p>
+          );
+        })} */}
+        
+        <form onSubmit={sendChat}>
+
+            <input 
+              type="text" 
+              name="chat" 
+              placeholder='send text' 
+              class="messageInput" 
+              value={message} 
+              onChange={(e)=>{
+              setMessage(e.target.value)
+            }}/>
+            <button class="sendBtn" type="submit">Send</button>
+
+        </form>
+
       </header>
     </div>
   );
